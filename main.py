28e7ce9cc6da160e9531768f98966169cffbbbfd
Riza Masykur <hanirizo@gmail.com>
2021-10-03 04:18:18 +0700

    as

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "325435 asu World"}
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
